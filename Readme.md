e9e203dc624a4eb1069664bb5a89c305f2452acd
khalid1200 <khalidabdula28@gmail.com>
2023-09-07 10:04:37 +0700

    Add new file index.js and Readme.md

---
+++ Readme.md
@@ -0,0 +1,7 @@
+**Introduction GIT**
+# Description
+GIT merupakan tools untuk membantu melakukan management project directory.
+## Initialize GIT
+Use command git init to activate git in your directory
+### Section 1.A.1
+
---
+++ index.js
@@ -0,0 +1,7 @@
+// untracked file : file yg belum pernah di monitoring
+// untuk add FILE TERTENTU ke staged area : git add namaFile.js
+// untuk add SATU FOLDER ke staged area : git add .
+// untuk mengeluarkan (remove) dari staged area : git rm --cached namaFile.js
+// untuk cek status : git status
+// untuk menyimpan : git commit -m "Add new file index.js and Readme.md"  (harus diberi keterangan ketika menyimpan, batas kata 100 kata)
+document.writeln("Hello World");
